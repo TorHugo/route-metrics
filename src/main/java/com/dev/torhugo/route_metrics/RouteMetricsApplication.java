058341fa3ab445b9d362bd48417ecf86d03a1c5e
torhugo <arruda.victorhugo@outlook.com>
2024-04-01 13:28:19 -0300

    Basic Project.

---
+++ src/main/java/com/dev/torhugo/route_metrics/RouteMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.torhugo.route_metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteMetricsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/torhugo/route_metrics/RouteMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.torhugo.route_metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteMetricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
