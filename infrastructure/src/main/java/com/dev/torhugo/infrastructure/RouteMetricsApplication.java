420093ecc99a26ff90c72d0050f9d9af128861e3
torhugo <arruda.victorhugo@outlook.com>
2024-04-22 12:39:06 -0300

    FIX (deploy): - Refactoring CD.

--- infrastructure/src/main/java/com/dev/torhugo/infrastructure/RouteMetricsApplication.java
+++ infrastructure/src/main/java/com/dev/torhugo/infrastructure/RouteMetricsApplication.java
@@ -1,11 +1,14 @@
 package com.dev.torhugo.infrastructure;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class RouteMetricsApplication {
     public static void main(String[] args) {
+        log.info("Starting RouteMetrics.");
         SpringApplication.run(RouteMetricsApplication.class, args);
     }
 
